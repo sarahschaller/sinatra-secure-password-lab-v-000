44878fd5ee32c8b252ddba15ab9b061d3eac4dcf
Sarah Schaller <sarcsch@aol.com>
2017-10-15 19:12:12 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    if params[:username] == params[:username] || params[:password] = params[:password]
+    if params[:username] == "" || params[:password] = ""
       redirect "/failure"
     else
       redirect "/login"
