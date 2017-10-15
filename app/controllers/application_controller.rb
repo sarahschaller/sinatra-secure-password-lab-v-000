6c2f760364bc017443a5eb1e418e289d875f38d5
Sarah Schaller <sarcsch@aol.com>
2017-10-15 19:03:59 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
     if params[:username] == "" || params[:password] = ""
       redirect "/failure"
     else
-      redirect to "/login"
+      redirect "/login"
     end
 
   end
